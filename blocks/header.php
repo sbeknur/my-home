05f48fc47ad377107fe943af2c0f0e99f22e2a2a
sbeknur2003@gmail.com <94599058+sbeknur@users.noreply.github.com>
2023-12-01 00:54:36 +0600

    start

---
+++ blocks/header.php
@@ -0,0 +1,20 @@
+<header class="header">
+    <div class="container">
+        <div class="header__inner">
+            <a href="#" class="header__logo"><img src="img/logo.svg" alt="logo" class="logo"></a>
+
+            <nav class="header__menu menu">
+                <ul class="menu__list">
+                    <li class="menu__item"><a href="#" class="menu__link">Начало работы</a></li>
+                    <li class="menu__item"><a href="#" class="menu__link">Гид по строительству и дизайну</a></li>
+                    <li class="menu__item"><a href="#" class="menu__link menu__link-icon">Поддержка</a></li>
+                </ul>
+            </nav>
+
+            <div class="header__buttons">
+                <button class="button button-download">Скачать</button>
+                <button class="button button-buy">Купить</button>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мой Дом 3D</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Nunito+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <?php include 'blocks/header.php'; ?>
+
+    <main></main>
+
+    <?php include 'blocks/footer.php'; ?>
+</body>
+
+</html>
