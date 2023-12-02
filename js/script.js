998665b0a65d7550b87047fec6d6db8433932442
sbeknur2003@gmail.com <94599058+sbeknur@users.noreply.github.com>
2023-12-02 16:43:58 +0600

    js

--- js/script.js
+++ js/script.js
@@ -4,13 +4,20 @@ $(document).ready(function () {
         infinite: true,
         autoplay: true,
         autoplaySpeed: 3000
-    });
+    })
+
+    $(function popup() {
+        $(".popup").click(function () {
+            $(".popup__content").toggleClass("popup__content-hidden");
+            $(".popup__play").toggleClass("popup__play-hidden");
+        })
+    })
 
     $(function dropdown() {
         $(".dropdown__item").each(function () {
             $(this).click(function () {
                 $(this).toggleClass("dropdown__item-hidden");
-                $(this).$(".dropdown__icon")
+                $(this).$(".dropdown__icon");
             })
         })
     })
